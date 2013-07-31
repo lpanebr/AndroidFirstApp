0bb137dfdf8c28e100bcd41f00d35c334af2a1fe
Luciano <lpanebr@gmail.com>
2013-07-30 23:40:54 -0300

    FIXES #1 - added menu entry - added string for menu entry - added onOptionsItemSelected method to the MainActivity

--- src/br/com/editoracubo/myfirstapp/MainActivity.java
+++ src/br/com/editoracubo/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -17,14 +18,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -33,5 +33,19 @@ public class MainActivity extends Activity {
     	intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_exit:
+            	Intent intent = new Intent(Intent.ACTION_MAIN);
+            	intent.addCategory(Intent.CATEGORY_HOME);
+            	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            	startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
